af8e80a97725678deb5a6e2f7ef20a55d716b28b
Harmandeep Gill <gillhimmy@gmail.com>
2023-11-14 19:51:01 -0500

    2.12 Exercise 3 complete Optimized dice rolls using a single IBAction button

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -11,37 +11,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var resultLabel: UILabel!
     
+    let dice = [4, 6, 8, 10, 12, 20, 100]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func fourSidedButtonPressed(_ sender: UIButton) {
-        resultLabel.text = "You rolled a 4 sided dice and got a \(Int.random(in: 1...4))"
-    }
-    
-    @IBAction func sixSidedButtonPressed(_ sender: UIButton) {
-        resultLabel.text = "You rolled a 6 sided dice and got a \(Int.random(in: 1...6))"
-    }
-    
-    @IBAction func eightSidedButtonPressed(_ sender: UIButton) {
-        resultLabel.text = "You rolled a 8 sided dice and got a \(Int.random(in: 1...8))"
-    }
-    
-    @IBAction func tenSidedButtonPressed(_ sender: UIButton) {
-        resultLabel.text = "You rolled a 10 sided dice and got a \(Int.random(in: 1...10))"
-    }
-    
-    @IBAction func twelveSidedButtonPressed(_ sender: UIButton) {
-        resultLabel.text = "You rolled a 12 sided dice and got a \(Int.random(in: 1...12))"
-    }
-    
-    @IBAction func twentySidedButtonPressed(_ sender: UIButton) {
-        resultLabel.text = "You rolled a 20 sided dice and got a \(Int.random(in: 1...20))"
-    }
-    
-    @IBAction func hundredSidedButtonPressed(_ sender: UIButton) {
-        resultLabel.text = "You rolled a 100 sided dice and got a \(Int.random(in: 1...100))"
+    @IBAction func diceButtonPressed(_ sender: UIButton) {
+        resultLabel.text = "You rolled a \(dice[sender.tag]) sided dice and got a \(Int.random(in: 1...dice[sender.tag]))"
     }
 }
 
